fca74e0515a58aee26d747129766dfc2e276c481
thibautRevilliod <thibaut.revilliod@gmail.com>
2015-03-22 12:14:21 +0100

    Add php-typography library
    Contains :
    php-typography library
    Mozilla trad files
    index.php to test the library with Mozilla files

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+include('php-typography.php');
+$typo = new phpTypography();
+
+$monfichier = fopen('cache_fr.php', 'r');
+
+// on passe les 2 première lignes du fichier
+$firstLine = fgets($monfichier); 
+$firstLine = fgets($monfichier); 
+
+// on lit la première ligne de donnée
+$firstLine = fgets($monfichier); // On lit la première ligne 
+
+// on récupère la traduction de la ligne courante
+$parseTrad = explode("=>", $firstLine);
+
+//on supprime les caractères indésirables
+$parseTrad = substr($parseTrad[1],2,-3); 
+// traduction corrigée
+$correctTrad = $typo->process($parseTrad);
+
+echo "Traduction originale :  ".$parseTrad."<br/>";
+echo "Traduction corrigée : ".$correctTrad;
+
+?>
