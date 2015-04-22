c4540f401779e5bfba1718be1d80cdf9151e48bb
LeaMiage <lea.cheoux@gmail.com>
2015-04-22 18:09:22 +0200

    Use GitHub API to create a Pull Request

--- app/controllers/insert.php
+++ app/controllers/insert.php
@@ -5,8 +5,9 @@ use Typolib\PullRequest;
 $pr = new PullRequest("Great feature");
 $pr->createNewBranch();
 
-$file_name = DATA_ROOT . 'typolib/data/test.php';
+$file_name = DATA_ROOT . 'typolib/test.php';
 // Update content in repository
 file_put_contents($file_name, "Règle 1\nRègle 2\n");
 
 $pr->commitAndPush();
+$pr->createPullRequest();
